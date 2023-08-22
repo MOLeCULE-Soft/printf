bf3a6857d16d14f39ffdcfd60b96d0fa42889690
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-22 12:16:01 +0100

    Tried fixing length handler

--- _printf.c
+++ _printf.c
@@ -180,7 +180,12 @@ int _printf(const char *format, ...)
 				case 'b':
 				case 'u':
 				case 'o':
-					params.UInt = va_arg(var_arg_list, unsigned int);
+					if (flag_set(&flags, 'l'))
+						params.UInt = va_arg(var_arg_list, unsigned long);
+					else if (flag_set(&flags, 'h'))
+						params.UInt = (short)va_arg(var_arg_list, unsigned int);
+					else
+						params.UInt = va_arg(var_arg_list, unsigned int);
 					if (format[j] == 'b')
 						base = 2;
 					else if (format[j] == 'o')
