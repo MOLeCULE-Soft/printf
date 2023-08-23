382089496330a92160c52e5f9aa2f193e01dcc1e
HusnaHussein <husnahussainkilalo@gmail.com>
2023-08-22 19:35:04 -0700

    Fixing code

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 int _printf(const char *format, ...);
 /**
 * _printf - prints formatted string to
-* the
+* the stdout
 * @format: format specifications or option
 * ...: [ARGUMENTS], varaible params
 *
