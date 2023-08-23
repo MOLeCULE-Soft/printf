abb2de67e9fd4aab66df50908347831bec6d7e8e
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-23 03:51:40 +0100

    Some cleaning up

--- test/cipherROT13.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-#include <limits.h>
-int main(void)
-{
-        _printf("%R : %d : %r", "We're gonna have an %c grade (100%%) in the %s project%!", 'A', "0x11. C - printf");
-        return (0);
-}
